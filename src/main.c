58a47a2bf7da5b123054c748592fc016199f7e26
Nicolas Cuadrado <nicolas.cuadrado@hotmail.com>
2018-05-22 01:48:12 -0500

    Serial communication processing OK
    Code for receiving incoming data from STPM33 is ok with transformation to dec and inversing the LSB logic, excluding CRC of the analysis.

--- src/main.c
+++ src/main.c
@@ -1,25 +1,39 @@
-/*
-	
-	Main program of Harvester ...
-	* 
-	* 
-	
-	ChibiOS - Copyright (C) 2006..2016 Giovanni Di Sirio
+/**
+  ******************************************************************************
+  * File Name          : main.c
+  * Description        : Main program body
+  ******************************************************************************
+  *
+  * COPYRIGHT(c) 2016 STMicroelectronics
+  *
+  * Redistribution and use in source and binary forms, with or without modification,
+  * are permitted provided that the following conditions are met:
+  *   1. Redistributions of source code must retain the above copyright notice,
+  *      this list of conditions and the following disclaimer.
+  *   2. Redistributions in binary form must reproduce the above copyright notice,
+  *      this list of conditions and the following disclaimer in the documentation
+  *      and/or other materials provided with the distribution.
+  *   3. Neither the name of STMicroelectronics nor the names of its contributors
+  *      may be used to endorse or promote products derived from this software
+  *      without specific prior written permission.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+  
+/* Includes ------------------------------------------------------------------*/
 
-	Licensed under the Apache License, Version 2.0 (the "License");
-	you may not use this file except in compliance with the License.
-	You may obtain a copy of the License at
-
-		http://www.apache.org/licenses/LICENSE-2.0
-
-	Unless required by applicable law or agreed to in writing, software
-	distributed under the License is distributed on an "AS IS" BASIS,
-	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-	See the License for the specific language governing permissions and
-	limitations under the License.
-	* 
-	* 
-*/
+#include <math.h>
 
 #include "ch.h"
 #include "hal.h"
@@ -73,6 +87,56 @@ static THD_FUNCTION(Thread2, arg) {
 
 }
 
+
+void getLSBFirstBinary(char * data, int binData[]){
+
+	int n, c, k, i;
+	
+	for(i = 0; i < sizeof(data); i++){
+		
+		chprintf((BaseChannel *)&SD3, "Value %d: %X.\n\r", i, data[i]);
+		
+		n = (int) data[i];
+        
+        for (c = 7; c >= 0; c--){
+			
+			k = n >> c;
+
+			if (k & 1){
+				binData[c+i*8] = 1;			
+			}
+			else {
+				binData[c+i*8] = 0;		
+			}	
+		}
+	
+	}
+	
+}
+
+unsigned long bin2Dec(int * array, int length){
+	
+	int i;
+	unsigned long sum = 0;
+	
+	for(i = 0; i < length; i++)
+		sum += array[i]*pow(2,i);
+		
+	return sum;
+		
+}
+
+void printIntArray(int * array, int length){
+	
+	int i;
+	
+	for(i = length-1; i >= 0; i--)
+		chprintf((BaseChannel *)&SD3, "%d", array[i]);
+	
+	chprintf((BaseChannel *)&SD3, "\n\r");	
+		
+}
+
 /*===========================================================================*/
 /* Initialization and main thread.                                           */
 /*===========================================================================*/
@@ -101,6 +165,7 @@ int main(void) {
     * PA9(TX) and PA10(RX) are routed to USART1.
     */
 
+    sdStart(&SD2, NULL);
     sdStart(&SD3, NULL);
 
     /*
@@ -117,12 +182,30 @@ int main(void) {
     */
 
     // Change to
+
     while (RUN_MAIN_THREAD) {
 		
 		// ...
-		chprintf((BaseChannel *)&SD3, "Waiting for BD confirmation \n\r");
-        chThdSleepMilliseconds(5000);
-	
+        
+        int rspValBin[32];
+        char rsp[5];
+        char testComm_1[] = "\x04\xff\xff\xff\x83";
+        
+        sdWrite(&SD2, (uint8_t *) testComm_1, 5);
+		
+		sdReadTimeout(&SD2, rsp, sizeof(rsp), 2000);
+        
+        getLSBFirstBinary(rsp, rspValBin);
+        
+        printIntArray(rspValBin, sizeof(rspValBin)/sizeof(rspValBin[0]));		
+        
+        unsigned long rspValDec = bin2Dec(rspValBin, sizeof(rspValBin)/sizeof(rspValBin[0]));
+        
+        chprintf((BaseChannel *)&SD3, "Value Dec: %lu \n\r", rspValDec);
+        
+        memset(rspValBin, 0, sizeof(rspValBin));
+        memset(rsp, 0, sizeof(rsp));
+        	
 	}
         
 }
